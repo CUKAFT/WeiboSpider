65e6eaed43ff4406133b17a944cf38b3d46b8243
CUKAFT <73210422+CUKAFT@users.noreply.github.com>
2021-01-23 23:17:41 -0500

    Update settings.py

--- weibospider/settings.py
+++ weibospider/settings.py
@@ -9,7 +9,7 @@ ROBOTSTXT_OBEY = False
 
 # change cookie to yours
 DEFAULT_REQUEST_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/61.0',
+    'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
     'Cookie': '_T_WM=2a582f9f00aecfd3630c9e5c44840c2d; WEIBOCN_WM=3349; H5_wentry=H5; backURL=https%3A%2F%2Fweibo.cn%2F; SUB=_2A25NCOVyDeRhGeFL7lYZ9ibLzDmIHXVu8os6rDV6PUJbktAfLVTskW1Nfef3VFRkfhfiYdr_uNDYcZiDgFPu9n_f; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5zqa.R3lO00qy66IZe3uSu5NHD95QNSK-X1hqRS0MfWs4DqcjMi--NiK.Xi-2Ri--ciKnRi-zNS0-fShnc1hMNSBtt; SSOLoginState=1611437346'}
 
 CONCURRENT_REQUESTS = 16
